474f9a959ed20a377345f9aec90e1a6952c9702f
Muhammad Hamza <146325213+MuhammadHamza7057@users.noreply.github.com>
2025-02-05 15:52:51 +0500

    Add files via upload
    I have gained a solid understanding of operators in JavaScript, including arithmetic, comparison, logical, assignment, and ternary operators. I have also explored how these operators play a vital role in manipulating values and performing essential operations, such as mathematical calculations, comparisons, and conditional logic within JavaScript programs.

---
+++ Day 3 Operator/script.js
@@ -0,0 +1,20 @@
+
+// In this code i use the 4 basic arthimetic operator 
+let a = 5;
+let b = 2;
+
+console.log("a = " + a, "b = " + b); // they print the actual value of the operand 
+console.log("a + b = ", a + b); // they add the value of a and b
+console.log("a - b = ", a - b); // they sub the value of a and b
+console.log("a * b = ", a * b); // they multiply the value of a and b
+console.log("a / b = ", a / b); // they divide the value of a and b and not like c++ they show the decimal point
+
+// now we have also use the exponential operator in js to calculate the value 
+console.log("a ^ b = ", a ** b); // they a^b value is 25
+
+// we have also the unary operator in js 
+// increment and decrement and they have further two two types pre-increment and post-decrement
+let c = 7;
+console.log("a++ ", a++); // the post-increment print the value is the same and after that line increment
+console.log("++c ", ++c); // the pre-increment increment the value first
+// same is for decrement 
